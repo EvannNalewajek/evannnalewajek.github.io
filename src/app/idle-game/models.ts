276dfd3c18c481f01d51e3625fb7fcaa209c5e26
Evann Nalewajek <evann.nalewajek@gmail.com>
2025-10-15 15:32:20 +0200

    Jeu Guilde : Ajout de modèles pour la progression de guilde, quêtes et recrutement

--- src/app/idle-game/models.ts
+++ src/app/idle-game/models.ts
@@ -29,3 +29,30 @@ export interface EnemyTemplate {
 export interface EnemyInstance extends EnemyTemplate {
     currentHealth: number; // Current health of the enemy instance
 }
+
+export interface Guild {
+    level: number;
+}
+
+export type QuestKind = "HuntCountByType"; // First idea for a quest system
+
+export interface QuestTemplate {    // 1st try
+    kind: QuestKind;
+    ennemyType: string; // Ennemy name
+    count: number;  // Number of kills to get
+    goldReward: number;
+}
+
+export interface QuestInstance extends QuestTemplate {
+    id: string;
+    acceptedAt?: number; // Timestamp
+    progress: number;   // current number of kills
+    completed: boolean;
+}
+
+export interface Recruit {
+    id: string;
+    name: string;
+    level: number;
+    stats: Stats;
+}
