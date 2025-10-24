8eec9e16f1b2d3c6300e2efdba4c5d3fb394220b
Evann Nalewajek <evann.nalewajek@gmail.com>
2025-10-24 09:28:43 +0200

    Correction erreur build : Prerender et Budget

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'pokedex', renderMode: RenderMode.Prerender },
+  { path: 'pokedex/:id', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender
