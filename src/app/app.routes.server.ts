c34207f7550c26be9db9aa2ed71aa98050fb4af4
Evann Nalewajek <evann.nalewajek@gmail.com>
2025-10-24 10:57:10 +0200

    Correction erreur build : Prerender pour la page type

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,6 +4,7 @@ export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
   { path: 'pokedex', renderMode: RenderMode.Prerender },
   { path: 'pokedex/:id', renderMode: RenderMode.Client },
+  { path: 'types/:slug', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender
