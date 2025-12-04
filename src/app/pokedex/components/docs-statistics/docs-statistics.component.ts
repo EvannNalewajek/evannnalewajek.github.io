2795364e54c35d666a932f199a9a56c79fb5b6ef
Evann Nalewajek <evann.nalewajek@gmail.com>
2025-12-04 15:24:32 +0100

    Pokédex : Amélioration de la navigation

--- src/app/pokedex/components/docs-statistics/docs-statistics.component.ts
+++ src/app/pokedex/components/docs-statistics/docs-statistics.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-docs-statistics',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './docs-statistics.component.html',
   styleUrls: ['./docs-statistics.component.scss'],
 })
